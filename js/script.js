a93375791deb7f728d2a7f0090a080ce46d13834
Guo Nan <guonan@GuodeAir.gateway>
2020-07-05 17:18:41 +1000

    Initial commit

---
+++ js/script.js
@@ -0,0 +1,12 @@
+$(document).ready(function () {
+  $("header").load("./header.html");
+  $("nav").load("./navbar.html");
+  $("footer").load("./footer.html");
+});
+/* 
+$(function(){
+$("header").load("./header.html");
+  $("nav").load("./navbar.html");
+  $("footer").load("./footer.html");
+})
+*/
